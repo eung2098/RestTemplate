7e2c4816745c08b0cef687425daa8132659a2aea
eung2098 <jeg8421@naver.com>
2021-10-28 18:45:08 +0900

    Rest-Template java file

--- client/src/main/java/com/example/client/service/RestTemplateService.java
+++ client/src/main/java/com/example/client/service/RestTemplateService.java
@@ -1,6 +1,11 @@
 package com.example.client.service;
 
+import com.example.client.dto.Req;
+import com.example.client.dto.UserRequest;
 import com.example.client.dto.UserResponse;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -36,4 +41,104 @@ public class RestTemplateService {
 
     }
 
+    // http://localhost:9090/api/server/user/{userId}/name/{userName}
+    public UserResponse post(){
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand(100,"hong")
+                .toUri();
+        System.out.println(uri);
+
+        // http body -> object -> object mapper -> json -> rest template -> http body json
+        UserRequest req= new UserRequest();
+        req.setName("hong");
+        req.setAge(20);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<UserResponse> response = restTemplate.postForEntity(uri, req, UserResponse.class);
+
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getHeaders());
+        System.out.println(response.getBody());
+
+        return response.getBody();
+    }
+
+    public UserResponse exchange(){
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand(100,"hong")
+                .toUri();
+        System.out.println(uri);
+
+        // http body -> object -> object mapper -> json -> rest template -> http body json
+        UserRequest req= new UserRequest();
+        req.setName("hong");
+        req.setAge(20);
+
+        RequestEntity<UserRequest> requestEntity = RequestEntity
+                .post(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("X-authorization", "abcd")
+                .header("custom-header", "12345")
+                .body(req);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<UserResponse> response = restTemplate.exchange(requestEntity, UserResponse.class);
+
+        return response.getBody();
+    }
+
+    public Req<UserResponse> genericExchange(){
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand(100,"hong")
+                .toUri();
+        System.out.println(uri);
+
+        // http body -> object -> object mapper -> json -> rest template -> http body json
+
+        UserRequest userRequest= new UserRequest();
+        userRequest.setName("hong");
+        userRequest.setAge(20);
+
+        Req<UserRequest> req = new Req<>();
+        req.setHeader(
+                new Req.Header()
+        );
+
+        req.setResBody(
+                userRequest
+        );
+
+
+        RequestEntity<Req<UserRequest>> requestEntity = RequestEntity
+                .post(uri)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("X-authorization", "abcd")
+                .header("custom-header", "12345")
+                .body(req);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+
+
+        ResponseEntity<Req<UserResponse>> response =
+                restTemplate.exchange(requestEntity,
+                        new ParameterizedTypeReference<>(){});
+
+        return response.getBody();
+    }
 }
--- server/src/main/java/com/example/server/controller/ServerApiController.java
+++ server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -1,11 +1,11 @@
 package com.example.server.controller;
 
+import com.example.server.dto.Req;
 import com.example.server.dto.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("api/server")
 public class ServerApiController {
@@ -17,6 +17,27 @@ public class ServerApiController {
         user.setAge(age);
 
         return user;
+    }
+
+    @PostMapping("user/{userId}/name/{userName}")
+    public Req<User> post(@RequestBody Req<User> user,
+                          @PathVariable int userId,
+                          @PathVariable String userName,
+                          @RequestHeader("X-authorization") String authorization,
+                          @RequestHeader("custom-header") String customHeader
+    ){
+        log.info("authorization : {}, custom: {}", authorization, customHeader);
+        log.info("userId : {}, userName: {}", userId, userName);
+        log.info("client : {}", user);
+
+        Req<User> response = new Req<>();
+        response.setHeader(
+                new Req.Header()
+        );
+
+        response.setResBody(user.getResBody());
+
+        return response;
     }
 
 }
